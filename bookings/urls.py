3e9c97b43faf809f8c5e408a3b99d1a4e5db8ce8
kevinbhingradiya2002-code <kevinbhingradiya2002@gmail.com>
2025-10-10 14:21:26 +1300

    Remove debug endpoints for production deployment

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from . import views
-from . import debug_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('debug/', debug_views.deployment_status, name='deployment_status'),
-    path('fix-db/', debug_views.fix_database, name='fix_database'),
-    path('add-rooms/', debug_views.add_sample_rooms, name='add_sample_rooms'),
     path('register/', views.register, name='register'),
     path('rooms/', views.room_list, name='room_list'),
     path('rooms/<int:room_id>/', views.room_detail, name='room_detail'),
